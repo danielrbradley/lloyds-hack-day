0aaddddde7f08417ab37a84394636386852afd62
Daniel Bradley <daniel@danielbradley.net>
2014-03-07 15:31:08 +0000

    Fix absolute urls

--- app/js/app.js
+++ app/js/app.js
@@ -9,19 +9,19 @@ recoRequestApp.config([
     function ($routeProvider) {
         $routeProvider.
             when('/home', {
-                templateUrl: '/app/partials/home.html',
+                templateUrl: 'app/partials/home.html',
                 controller: 'HomeCtrl'
             }).
             when('/login', {
-                templateUrl: '/app/partials/login.html',
+                templateUrl: 'app/partials/login.html',
                 controller: 'LoginCtrl'
             }).
             when('/create-order', {
-                templateUrl: '/app/partials/create-order.html',
+                templateUrl: 'app/partials/create-order.html',
                 controller: 'CreateOrderCtrl'
             }).
             when('/order-recieved', {
-                templateUrl: '/app/partials/order-recieved.html',
+                templateUrl: 'app/partials/order-recieved.html',
                 controller: 'OrderRevievedCtrl'
             }).
             otherwise({
