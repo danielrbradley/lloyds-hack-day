7c8d86cca3a3b8a1ea9a11f1c57ce3aaac41e6af
Daniel Bradley <daniel@danielbradley.net>
2014-03-07 11:10:40 +0000

    Style order creation

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -30,10 +30,48 @@ controllers.controller('HomeCtrl', [
 controllers.controller('CreateOrderCtrl', [
     '$scope', '$routeParams', '$location', 'userService',
     function ($scope, $routeParams, $location, userService) {
+        $scope.pennies = 0;
+        $scope.twoPennies = 0;
+        $scope.fivePennies = 0;
+        $scope.tenPennies = 0;
+        $scope.twentyPennies = 0;
+        $scope.fiftyPennies = 0;
+        $scope.onePound = 0;
+        $scope.twoPounds = 0;
+        $scope.fivePounds = 0;
+        $scope.tenPounds = 0;
+        $scope.twentyPounds = 0;
+        $scope.fiftyPounds = 0;
+        $scope.getTotal = function() {
+            return parseInt($scope.pennies) +
+                parseInt($scope.twoPennies) +
+                parseInt($scope.fivePennies) +
+                parseInt($scope.tenPennies) +
+                parseInt($scope.twentyPennies) +
+                parseInt($scope.fiftyPennies) +
+                parseInt($scope.onePound) +
+                parseInt($scope.twoPounds) +
+                parseInt($scope.fivePounds) +
+                parseInt($scope.tenPounds) +
+                parseInt($scope.twentyPounds) +
+                parseInt($scope.fiftyPounds);
+        };
+        $scope.getWeight = function() {
+            return Math.round((0.055 * $scope.twentyPounds)) / 1000;
+        };
         $scope.create = function () {
             var order = {
-                pennies: $scope.pennies, twoPennies: $scope.twoPennies, fivePennies: $scope.fivePennies, tenPennies: $scope.tenPennies, fiftyPennies: $scope.fiftyPennies,
-                onePound: $scope.onePound, twoPounds: $scope.twoPounds, fivePounds: $scope.fivePounds, tenPounds: $scope.tenPounds, twentyPounds: $scope.twentyPounds,
+                pennies: $scope.pennies,
+                twoPennies: $scope.twoPennies,
+                fivePennies: $scope.fivePennies,
+                tenPennies: $scope.tenPennies,
+                twentyPennies: $scope.twentyPennies,
+                fiftyPennies: $scope.fiftyPennies,
+                onePound: $scope.onePound,
+                twoPounds: $scope.twoPounds,
+                fivePounds: $scope.fivePounds,
+                tenPounds: $scope.tenPounds,
+                twentyPounds: $scope.twentyPounds,
                 fiftyPounds: $scope.fiftyPounds
             };
             var userDataRef = new Firebase('https://amber-fire-7123.firebaseio.com/users/' + userService.get().username);
