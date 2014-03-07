1c985ca8f7ad0cc24ba99f1ae8f6902cc5050343
Mike <mikrid88@msn.com>
2014-03-07 13:07:38 +0000

    Branch stuff

---
+++ public/branch-app/js/app.js
@@ -0,0 +1,17 @@
+﻿var recoRequestApp = angular.module('cashOrdersApp', [
+    'ngRoute',
+    'cashOrdersControllers',
+    'cashOrdersServices'
+]);
+
+recoRequestApp.config([
+    '$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+            when('/branch-home', {
+                templateUrl: '/branch-app/partials/orders-overview.html',
+                controller: 'ViewOrderCtrl'
+            })
+		}
+	
+]);
---
+++ public/branch-app/js/controllers.js
@@ -0,0 +1,15 @@
+﻿/// <reference path="_references.js" />
+/// <reference path="services.js" />
+var controllers = angular.module('cashOrdersControllers', []);
+
+controllers.controller('ViewOrderCtrl', [
+    '$scope', '$routeParams', '$location', 'userService',
+    function ($scope, $routeParams, $location, userService) {
+
+			var branchDataRef = new Firebase('https://amber-fire-7123.firebaseio.com/branches/' + $scope.branch);
+			var allOrders = branchDataRef.on('value', function(dataSnapshot){});
+			document.write(allOrders.toString());
+		
+    }
+]);
+
